ff5881115db1c9bee7301bbf40975f4b43347f00
Dario Piotrowicz <dario@cloudflare.com>
2023-11-28 11:56:30 +0000

    add comment

--- poc/src/api-shared.ts
+++ poc/src/api-shared.ts
@@ -2,6 +2,10 @@ import type { KVNamespace } from '@cloudflare/workers-types';
 
 export type RequestContext = {
     req: any,
+    // Note: here the MY_KV type is hardcoded, maybe we could do something clever
+    //       and get the types correctly inferred in dev mode (with a dev cli option?)
+    //       otherwise this env object could be a Record<string, Binding> or something
+    //       like that
     env: { MY_KV: KVNamespace },
     ctx: any
 };
