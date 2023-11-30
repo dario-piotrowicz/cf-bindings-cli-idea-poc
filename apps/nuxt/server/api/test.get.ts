dc804ed85600dde7fd460328fbc80d0698b7ad99
Dario Piotrowicz <dario@cloudflare.com>
2023-11-30 10:09:50 +0000

    implement idea

--- apps/nuxt/server/api/test.get.ts
+++ apps/nuxt/server/api/test.get.ts
@@ -1,9 +1,7 @@
-import { getRequestContext } from 'poc';
+import { KVS } from 'poc';
 
 export default defineEventHandler(async (event) => {
-    const { env } = await getRequestContext();
-
-    const numOfKeys = (await env.MY_KV.list()).keys.length;
-
+    const numOfKeys = (await KVS.MY_KV.list()).keys.length;
+ 
     return `The number of keys in env.MY_KV is: ${numOfKeys}`;
 });
--- poc/src/api.dev.ts
+++ poc/src/api.dev.ts
@@ -1,6 +1,12 @@
+import { KVNamespace } from '@cloudflare/workers-types';
 import { _getRequestContext, _setRequestContext, type RequestContext } from './api-shared.js';
 
+let kvs: Record<string, KVNamespace> = {};
+
 export async function getRequestContext(): Promise<RequestContext> {
+    // Note: this is hardcoded here, in theory it would be read from a toml file
+    const kvsNames = ['MY_KV'];
+
     const reqCtx = _getRequestContext();
 
     if(!reqCtx) {
@@ -9,12 +15,18 @@ export async function getRequestContext(): Promise<RequestContext> {
             modules: true,
             script: '',
             // the bindings should be read from a wrangler.toml file
-            kvNamespaces: ['MY_KV']
+            kvNamespaces: kvsNames
         });
     
+        const bindings = await mf.getBindings();
+
+        for(const kvName of kvsNames) {
+            kvs[kvName] = bindings[kvName] as any;
+        }
+
         const newReqCtx: RequestContext = {
             req: null,
-            env: await mf.getBindings(),
+            env: bindings as any,
             ctx: null,
         };
 
@@ -24,3 +36,7 @@ export async function getRequestContext(): Promise<RequestContext> {
 
     return reqCtx;
 }
+
+await getRequestContext();
+
+export const KVS = kvs;
